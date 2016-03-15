38a8b1adea2094a052975dff8b9ecb2373ee577c
chenglei <chenglei.1986@163.com>
2016-03-15 11:45:55 +0800

    上传代码

---
+++ Android-EDP-Sample/src/com/chinamobile/iot/onenet/edp/sample/Config.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2015. China Mobile IOT. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.chinamobile.iot.onenet.edp.sample;
+
+/**
+ * Created by chenglei on 2015/12/24.
+ */
+public class Config {
+
+    public static final String EDP_HOST = "jjfaedp.hedevice.com";
+
+    public static final int EDP_PORT_876 = 876;
+
+    public static final int EDP_PORT_29876 = 29876;
+
+}
